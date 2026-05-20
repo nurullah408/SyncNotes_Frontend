ec5165a49bedb8c2b692fc8de9f15c4e8adc260f
nurullah408 <nurullahan408@gmail.com>
2026-05-20 22:56:47 +0600

    Initial commit for integrating syncing with API

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,11 @@
+export function useLocalStorage() {
+  const setItem = (key: string, value: string) => {
+    localStorage.setItem(key, value);
+  };
+
+  const getItem = (key: string) => {
+    return localStorage.getItem(key);
+  };
+
+  return { setItem, getItem };
+}
--- src/types/Note.ts
+++ src/types/Note.ts
@@ -3,4 +3,5 @@ export interface Note {
   title: string;
   content: string;
   lastUpdated: string;
+  isDeleted?: boolean;
 }
